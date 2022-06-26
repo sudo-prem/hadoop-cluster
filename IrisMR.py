2f4874134787a3952b2e04fae172352f264ee60c
premnaaath <premnaathvaradharajan@gmail.com>
2022-06-26 22:22:52 +0530

    Update IrisMR.py

--- IrisMR.py
+++ IrisMR.py
@@ -1,26 +1,27 @@
 from mrjob.job import MRJob
+from mrjob.step import MRStep
 
 class IrisMR(MRJob):
+    def step(self):
+        return [MRStep(mapper = self.mapper, reducer = self.reducer)]
+
     def mapper(self, _, line):
-      _, month, _, _, temperature = line.split()
-      
-      yield (month, (temperature, 1))
-      
-    def _reducer_combiner(self, month, temperatures):
-      avg, count = 0, 0
-      for tmp, c in temperatures
-        avg = (avg * count + tmp * c) / (count + c)
-        count += c
-      return (month, (avg, count))
-      
-    def combiner(self, month, temperatures):
-      yield self._reducer_combiner(month, temperatures)
-      
-    def reducer(self, month, temperatures):
-      month, (avg, count) = self._reducer_combiner(month, temperatures)
-      
-      # (May, 28 Degrees)
-      yield (month, avg)
+        (sl,sw,pl,pw,category) = line.split(',')
+        yield (category, (sl, 1))
+
+    def _reducer_combiner(self, category, value_count):
+        avg, cnt = 0, 0
+        for val, c in value_cnt:
+            avg = (avg * cnt + val * c) / (cnt + c)
+            cnt += c
+        return (category, (avg, cnt))
+
+    def combiner(self, category, value_count):
+        yield self._reducer_combiner(category, value_count)
+
+    def reducer(self, category, value_count):
+        category, (avg, count) = self._reducer_combiner(category, value_count)
+        yield (category, avg)
 
 if __name__ == '__main__':
-     IrisMR.run()
+    IrisMR.run()
