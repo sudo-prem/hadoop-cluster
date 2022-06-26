80e0f60071db41ff400ad464dd03ae8fad91967a
premnaaath <premnaathvaradharajan@gmail.com>
2022-06-26 23:59:59 +0530

    Final Commit

--- IrisMR.py
+++ IrisMR.py
@@ -1,13 +1,23 @@
 from mrjob.job import MRJob
 from mrjob.step import MRStep
 
+choice = "sepal_length"
+
+
 class IrisMR(MRJob):
     def step(self):
         return [MRStep(mapper=self.mapper, reducer=self.reducer)]
 
     def mapper(self, _, line):
-        (sl,sw,pl,pw,category) = line.split(',')
-        yield (category, (float(sl), 1))
+        (sl, sw, pl, pw, category) = line.split(',')
+        if choice == "sepal_length":
+            yield (category, (float(sl), 1))
+        elif choice == "sepal_width":
+            yield (category, (float(sw), 1))
+        elif choice == "petal_length":
+            yield (category, (float(pl), 1))
+        else:
+            yield (category, (float(pw), 1))
 
     def _reducer_combiner(self, category, value_count):
         avg, cnt = 0, 0
@@ -22,6 +32,7 @@ class IrisMR(MRJob):
     def reducer(self, category, value_count):
         category, (avg, count) = self._reducer_combiner(category, value_count)
         yield (category, avg)
+
 
 if __name__ == '__main__':
     IrisMR.run()
